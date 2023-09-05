4e79a562e7490ee3dc489e7ad19d700ed546f685
Silvio Ruiz <silviojrck@gmail.com>
2023-09-05 19:59:59 -0300

    Agrega anotaciones de swagger para documentar create product dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsInt,
   IsNumber,
@@ -9,32 +10,65 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'Product Title (unique)',
+    nullable: false,
+    uniqueItems: true,
+    minLength: 1,
+    maxLength: 100,
+  })
   @IsString()
   @MinLength(1)
-  @MaxLength(30)
+  @MaxLength(100)
   name: string;
 
+  @ApiProperty({
+    description: 'Product Description',
+    nullable: true,
+    minLength: 5,
+    maxLength: 200,
+  })
   @IsString()
   @IsOptional()
   @MinLength(5)
   @MaxLength(200)
   description: string;
 
+  @ApiProperty({
+    description: 'Product Price (float)',
+    nullable: true,
+  })
   @IsNumber()
   @IsPositive()
   @IsOptional()
   price: number;
 
+  @ApiProperty({
+    description: 'Product stock',
+    nullable: false,
+    minimum: 0,
+  })
   @IsNumber()
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock: number;
 
+  @ApiProperty({
+    description: 'Product slug',
+    nullable: true,
+    minimum: 0,
+    uniqueItems: true,
+  })
   @IsString()
   @IsOptional()
   slug?: string;
 
+  @ApiProperty({
+    description: 'Product url imagen',
+    nullable: false,
+    minimum: 0,
+  })
   @IsOptional()
   @MinLength(5)
   @MaxLength(200)
