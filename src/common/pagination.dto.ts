67147a299b985f6b97b025825e4e8f3b1835eda4
Silvio Ruiz <silviojrck@gmail.com>
2023-09-05 20:58:46 -0300

    Añade documentacion swagger pagination.dto

--- src/common/pagination.dto.ts
+++ src/common/pagination.dto.ts
@@ -1,7 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsOptional, IsPositive, Max, Min } from 'class-validator';
 
 export class PaginationDto {
+  @ApiProperty({
+    description: 'Maximum number of items to display per page',
+    minimum: 0,
+    maximum: 100,
+    default: 10, // You can define a default value if desired
+    required: false, // Indicates that it's not necessary to provide this field in the request
+  })
   @IsOptional()
   @IsPositive()
   @Min(0)
@@ -9,6 +17,12 @@ export class PaginationDto {
   @Type(() => Number)
   limit?: number;
 
+  @ApiProperty({
+    description: 'Number of items to skip before starting to list',
+    minimum: 0,
+    default: 0, // You can define a default value if desired
+    required: false, // Indicates that it's not necessary to provide this field in the request
+  })
   @IsOptional()
   @IsPositive()
   @Min(0)
