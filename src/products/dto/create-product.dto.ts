169085a62c1db11f405185762dd58dd31bb732e0
Silvio Ruiz <silviojrck@gmail.com>
2023-09-05 19:18:13 -0300

    Agrega class validator y lo implementa en product.dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,7 +1,42 @@
+import {
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
+  @MaxLength(30)
   name: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(5)
+  @MaxLength(200)
   description: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
   price: number;
+
+  @IsNumber()
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
   stock: number;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsOptional()
+  @MinLength(5)
+  @MaxLength(200)
   image: string;
 }
