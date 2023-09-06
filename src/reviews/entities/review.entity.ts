4a5f7a82423d34afda97b8530071c9893432bc72
Silvio Ruiz <silviojrck@gmail.com>
2023-09-06 00:32:21 -0300

    Define como se verá la información que llegue del front para crear una review

--- src/reviews/entities/review.entity.ts
+++ src/reviews/entities/review.entity.ts
@@ -1,1 +1,26 @@
-export class Review {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Review {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'text',
+    length: 15,
+  })
+  authorName: string;
+
+  @Column({
+    type: 'text',
+    length: 150,
+  })
+  @Column('text')
+  comment: string;
+
+  @Column('date')
+  publicationDate: Date;
+
+  @Column('int')
+  rating: number;
+}
