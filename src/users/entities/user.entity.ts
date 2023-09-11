15d51b99170ce1dd34639b97822cc6caa4560975
Silvio Ruiz <silviojrck@gmail.com>
2023-09-11 18:49:59 -0300

    Agrega entidad de usuario y enumeracion de roles

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,44 @@
-export class User {}
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Role } from '../enums/user-role.enum';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  email: string;
+
+  @Column('text', {
+    select: false,
+  })
+  password: string;
+
+  @Column('text')
+  name: string;
+
+  @Column('text')
+  surname: string;
+
+  @Column({ type: 'enum', enum: Role, default: Role.USER })
+  role: Role;
+
+  @Column('bool', {
+    default: true,
+  })
+  isActive: boolean;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  checkFields() {
+    this.email = this.email.toLowerCase().trim();
+  }
+}
