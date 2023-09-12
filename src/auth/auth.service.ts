f1fcfc9eafd611709d75d160ed21058e7d105d11
Silvio Ruiz <silviojrck@gmail.com>
2023-09-12 16:18:48 -0300

    Importa el UserService en el authmodule

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginDto } from './dto/login.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly userService: UsersService) {}
   register(registerUserDto: RegisterUserDto) {
     return 'This action adds a new auth';
   }
