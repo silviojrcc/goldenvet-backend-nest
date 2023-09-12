c1b198bb38661d35e7271f5ae2ef1dbb02dbad1d
Silvio Ruiz <silviojrck@gmail.com>
2023-09-12 18:08:03 -0300

    Crea register user dto

--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -1,1 +1,58 @@
-export class RegisterUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { Role } from 'src/users/enums/user-role.enum';
+
+export class RegisterUserDto {
+  @ApiProperty({
+    description: 'User email (unique)',
+    nullable: false,
+    uniqueItems: true,
+  })
+  @IsEmail()
+  @IsString()
+  email: string;
+
+  @ApiProperty({
+    description: 'User password',
+    nullable: false,
+  })
+  @IsString()
+  @IsStrongPassword()
+  password: string;
+
+  @ApiProperty({
+    description: 'User name',
+    nullable: false,
+    maxLength: 50,
+  })
+  @IsString()
+  @MinLength(1)
+  @MaxLength(50)
+  name: string;
+
+  @ApiProperty({
+    description: 'User surname',
+    nullable: false,
+    maxLength: 50,
+  })
+  @IsString()
+  @MinLength(1)
+  @MaxLength(50)
+  surname: string;
+
+  @ApiProperty({
+    description: 'User roole',
+    nullable: false,
+  })
+  @IsEnum(Role)
+  @IsOptional()
+  role?: Role;
+}
