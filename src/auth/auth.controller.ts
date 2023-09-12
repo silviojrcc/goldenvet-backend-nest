e00aa32ce0c03a0760df0e5123ba7bd897321ee4
Silvio Ruiz <silviojrck@gmail.com>
2023-09-12 19:59:05 -0300

    Documenta el controller de auth con swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,16 +2,56 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginDto } from './dto/login.dto';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { User } from 'src/users/entities/user.entity';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiResponse({
+    status: 201,
+    description: 'User has been successfully registered',
+    type: User,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request: The request data is invalid',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Conflict: Email is already registered',
+  })
+  @ApiResponse({
+    status: 500,
+    description:
+      'Internal Server Error: An unexpected error occurred on the server',
+  })
   @Post('register')
   register(@Body() registerUserDto: RegisterUserDto) {
     return this.authService.register(registerUserDto);
   }
 
+  @ApiResponse({
+    status: 200,
+    description: 'Login successful: User has been authenticated',
+    type: User, // Reemplaza 'User' con el tipo de objeto que representa un usuario en tu aplicación
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request: The request data is invalid',
+  })
+  @ApiResponse({
+    status: 401,
+    description:
+      'Unauthorized: Incorrect credentials or authentication is required to perform this action',
+  })
+  @ApiResponse({
+    status: 500,
+    description:
+      'Internal Server Error: An unexpected error occurred on the server',
+  })
   @Post('login')
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
