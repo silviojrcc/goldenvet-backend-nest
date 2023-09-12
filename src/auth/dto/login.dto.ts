26de49bebcf21c265c36aa9af4cd7065d7c57cf0
Silvio Ruiz <silviojrck@gmail.com>
2023-09-12 17:37:37 -0300

    Crea dto para el login

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,1 +1,11 @@
-export class LoginDto {}
+import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
+
+export class LoginDto {
+  @IsEmail()
+  @IsString()
+  email: string;
+
+  @IsString()
+  @IsStrongPassword()
+  password: string;
+}
