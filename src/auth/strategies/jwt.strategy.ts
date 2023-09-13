2718d4315977c8579a6e858babdf08dccba94654
Silvio Ruiz <silviojrck@gmail.com>
2023-09-12 23:25:29 -0300

    Crea jwt strategy para validar los usuarios

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Repository } from 'typeorm';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { User } from 'src/users/entities/user.entity';
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    configService: ConfigService,
+  ) {
+    super({
+      secretOrKey: configService.get('JWT_SECRET'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { id } = payload;
+    const user = await this.userRepository.findOneBy({ id: id });
+
+    if (!user) throw new UnauthorizedException('Token is not valid');
+
+    if (!user.isActive) throw new UnauthorizedException('User is not active');
+
+    return user;
+  }
+}
