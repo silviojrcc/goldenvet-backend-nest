1af5dbf9be8de4f83bb748538adfee194583bb9c
Silvio Ruiz <silviojrck@gmail.com>
2023-09-28 19:51:33 -0300

    Crea dto de appointments

--- src/appointments/dto/create-appointment.dto.ts
+++ src/appointments/dto/create-appointment.dto.ts
@@ -1,1 +1,18 @@
-export class CreateAppointmentDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsString } from 'class-validator';
+
+export class CreateAppointmentDto {
+  @ApiProperty({
+    description: 'Visit detail',
+    nullable: false,
+  })
+  @IsString()
+  visitDetail: string;
+
+  @ApiProperty({
+    description: 'Appointment date',
+    nullable: false,
+  })
+  @IsDate()
+  appointmentDate: Date;
+}
