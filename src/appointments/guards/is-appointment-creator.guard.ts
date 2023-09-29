1dab2391c640960aa3318c392e176e9bbe34d6b5
Silvio Ruiz <silviojrck@gmail.com>
2023-09-29 01:06:34 -0300

    Crea guard para verificar si un usuario es creador de un appointment

---
+++ src/appointments/guards/is-appointment-creator.guard.ts
@@ -0,0 +1,41 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
+import { AppointmentsService } from '../appointments.service';
+import { ValidRoles } from 'src/auth/interfaces/valid-roles.interface';
+
+@Injectable()
+export class IsCreatorGuard implements CanActivate {
+  constructor(private readonly appointmentService: AppointmentsService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const { user, params } = context.switchToHttp().getRequest();
+
+    if (!params) throw new BadRequestException('Appointment id not found');
+
+    if (!user) throw new UnauthorizedException('User not found in request');
+
+    if (user.role === ValidRoles.ADMIN) return true;
+
+    const { id: appointmentId } = params;
+    const appointment = await this.searchAppointment(appointmentId);
+
+    if (!appointment) throw new NotFoundException('Appointment not found');
+
+    if (appointment.patient.id !== user.id)
+      throw new UnauthorizedException(
+        'User is not the creator of this appointment',
+      );
+
+    return true;
+  }
+
+  private async searchAppointment(appointmentId: string) {
+    return await this.appointmentService.findOne(appointmentId);
+  }
+}
